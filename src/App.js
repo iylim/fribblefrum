d8fed832d7e397b17403c564882079879f396ebb
Ivy Lim <yunjilim@gmail.com>
2018-08-24 11:21:26 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Fribble Frum</h1>
+        <div className="login">Login to connect</div>
+      </div>
+    );
+  }
+}
+
+export default App;
